960e3087a5e623f869b64057a610e8c719c7d932
ipaulo <627453+ipaulo@users.noreply.github.com>
2025-04-13 15:56:53 -0700

    Rename player name and scores
    Ruff

--- python/tennis3.py
+++ python/tennis3.py
@@ -1,27 +1,41 @@
 class TennisGame3:
     def __init__(self, player1_name, player2_name):
-        self.p1_n = player1_name
-        self.p2_n = player2_name
-        self.p1 = 0
-        self.p2 = 0
+        self.player1_name = player1_name
+        self.player2_name = player2_name
+        self.player1_score = 0
+        self.player2_score = 0
 
     def won_point(self, n):
         if n == "player1":
-            self.p1 += 1
+            self.player1_score += 1
         else:
-            self.p2 += 1
+            self.player2_score += 1
 
     def score(self):
-        if (self.p1 < 4 and self.p2 < 4) and (self.p1 + self.p2 < 6):
+        if (self.player1_score < 4 and self.player2_score < 4) and (
+            self.player1_score + self.player2_score < 6
+        ):
             p = ["Love", "Fifteen", "Thirty", "Forty"]
-            s = p[self.p1]
-            return s + "-All" if (self.p1 == self.p2) else s + "-" + p[self.p2]
+            s = p[self.player1_score]
+            return (
+                s + "-All"
+                if (self.player1_score == self.player2_score)
+                else s + "-" + p[self.player2_score]
+            )
         else:
-            if self.p1 == self.p2:
+            if self.player1_score == self.player2_score:
                 return "Deuce"
-            s = self.p1_n if self.p1 > self.p2 else self.p2_n
+            s = (
+                self.player1_name
+                if self.player1_score > self.player2_score
+                else self.player2_name
+            )
             return (
                 "Advantage " + s
-                if ((self.p1 - self.p2) * (self.p1 - self.p2) == 1)
+                if (
+                    (self.player1_score - self.player2_score)
+                    * (self.player1_score - self.player2_score)
+                    == 1
+                )
                 else "Win for " + s
             )
