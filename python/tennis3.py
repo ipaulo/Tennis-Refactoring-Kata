afb4df4471e4187486f315c081082962c482262d
ipaulo <627453+ipaulo@users.noreply.github.com>
2025-04-13 16:53:43 -0700

    Extract simple scoring

--- python/tennis3.py
+++ python/tennis3.py
@@ -11,24 +11,27 @@ class TennisGame3:
         else:
             self.player2_score += 1
 
-    def no_winner_yet(self) -> bool:
+    def no_winner(self) -> bool:
         if self.player1_score < 4 and self.player2_score < 4:
             return True
 
-    def is_basic_scoring(self) -> bool:
+    def is_simple_score(self) -> bool:
         if self.player1_score + self.player2_score == 6:
             return False
         return True
 
+    def simple_score(self) -> str:
+        score_list = ["Love", "Fifteen", "Thirty", "Forty"]
+        score_name = score_list[self.player1_score]
+        return (
+            score_name + "-All"
+            if (self.player1_score == self.player2_score)
+            else score_name + "-" + score_list[self.player2_score]
+        )
+
     def score(self):
-        if self.no_winner_yet() and self.is_basic_scoring():
-            score_list = ["Love", "Fifteen", "Thirty", "Forty"]
-            score_name = score_list[self.player1_score]
-            return (
-                score_name + "-All"
-                if (self.player1_score == self.player2_score)
-                else score_name + "-" + score_list[self.player2_score]
-            )
+        if self.no_winner() and self.is_simple_score():
+            return self.simple_score()
         else:
             if self.player1_score == self.player2_score:
                 return "Deuce"
