ed2e43a3a5fbe4de34f09608c9de97a2be21fe8a
ipaulo <627453+ipaulo@users.noreply.github.com>
2025-04-14 07:45:45 -0700

    Add typehints wherever possible

--- python/tennis3.py
+++ python/tennis3.py
@@ -1,12 +1,12 @@
 class TennisGame3:
-    def __init__(self, player1_name, player2_name):
-        self.player1_name = player1_name
-        self.player2_name = player2_name
-        self.player1_score = 0
-        self.player2_score = 0
+    def __init__(self, player1_name: str, player2_name: str):
+        self.player1_name: str = player1_name
+        self.player2_name: str = player2_name
+        self.player1_score: int = 0
+        self.player2_score: int = 0
 
-    def won_point(self, n):
-        if n == "player1":
+    def won_point(self, winner: str) -> None:
+        if winner == "player1":
             self.player1_score += 1
         else:
             self.player2_score += 1
@@ -16,13 +16,13 @@ class TennisGame3:
             return True
 
     def is_simple_score(self) -> bool:
-        """Check tied at Forty-Forty"""
+        """Check score is less than tied at Forty-Forty"""
         if self.player1_score + self.player2_score < 6:
             return True
         return False
 
     def simple_score(self) -> str:
-        score_list = ["Love", "Fifteen", "Thirty", "Forty"]
+        score_list: list[str] = ["Love", "Fifteen", "Thirty", "Forty"]
         score_name = score_list[self.player1_score]
         return (
             score_name + "-All"
@@ -33,10 +33,11 @@ class TennisGame3:
     def score(self):
         if self.no_winner() and self.is_simple_score():
             return self.simple_score()
+        # It is either deuce or advantage somebody
         elif self.player1_score == self.player2_score:
             return "Deuce"
         else:
-            leader_name = (
+            leader_name: str = (
                 self.player1_name
                 if self.player1_score > self.player2_score
                 else self.player2_name
